e19ba0ed95f73e50508476d0aff0bceff2182bd4
owen-0101 <owen_0101@163.com>
2025-04-09 22:29:12 +0800

    feat: add all token prices hook

--- src/hooks/useTokenPrice.tsx
+++ src/hooks/useTokenPrice.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 
 import { get_all_tokens } from '@/canister/icpswap/apis';
-import { get_token_price_ic_by_canister_id } from '@/components/api/price';
+import { TokenInfo } from '@/canister/swap/swap.did.d';
+import { get_token_price_ic, get_token_price_ic_by_canister_id } from '@/components/api/price';
+import { useTokenStore } from '@/stores/token';
 
 export const useTokenPrice = (canisterId: string | undefined, decimals: number = 8) => {
     const [priceData, setPriceData] = useState<{
@@ -50,3 +52,53 @@ export const useTokenPrice = (canisterId: string | undefined, decimals: number =
 
     return priceData;
 };
+
+export type TokenPrice = {
+    price?: number;
+    price_change_24h?: number;
+};
+
+export type TokenPriceInfo = TokenPrice & TokenInfo;
+
+export const useAllTokenPriceAndChange = () => {
+    const { tokenList } = useTokenStore();
+    const [priceData, setPriceData] = useState<TokenPriceInfo[] | undefined>();
+
+    useEffect(() => {
+        if (!tokenList?.length) {
+            setPriceData(undefined);
+            return;
+        }
+
+        const fetchData = async () => {
+            try {
+                // Fetch all price
+                const allTokenPrices = await get_token_price_ic();
+
+                const results = tokenList.map((token) => {
+                    const canisterId = token.canister_id.toString();
+                    const matchedToken = allTokenPrices.find((item) => item.canister_id === canisterId);
+
+                    return {
+                        ...token,
+                        price: matchedToken?.price ? Number(matchedToken.price) : undefined,
+                        price_change_24h: matchedToken?.price_change_24h
+                            ? Number(matchedToken.price_change_24h)
+                            : undefined,
+                    };
+                });
+
+                setPriceData(results);
+            } catch (error) {
+                console.error('Failed to fetch token prices:', error);
+                setPriceData([]);
+            }
+        };
+
+        fetchData();
+    }, [tokenList]);
+
+    return priceData;
+};
+
+// todo user token price and balance
