50ea92ea5a044ec3d07bba49312b9d046016887d
owen-0101 <owen_0101@163.com>
2025-04-10 17:29:42 +0800

    feat: add tokens balance and wallet balance

--- src/canister/swap/apis.ts
+++ src/canister/swap/apis.ts
@@ -1,4 +1,9 @@
 import { getAnonymousActorCreatorByAgent } from '@/components/connect/creator';
+import { ConnectedIdentity } from '@/types/identity.js';
+import { bigint2string } from '@/utils/common/bigint.ts';
+import { wrapOptionMap } from '@/utils/common/options.ts';
+import { string2principal } from '@/utils/common/principal.ts';
+import { hex2array } from '@/utils/hex.ts';
 
 import { _SERVICE, TokenInfo } from './swap.did.d';
 import { idlFactory } from './swap.did.ts';
@@ -14,4 +19,37 @@ export const get_tokens_query = async () => {
         console.error('Failed to fetch token list:', error);
         throw new Error(`Token query failed: ${error instanceof Error ? error.message : String(error)}`);
     }
+};
+
+// wallet balance
+export const get_wallet_balance = async (identity: ConnectedIdentity): Promise<string> => {
+    const { creator } = identity;
+    const create: _SERVICE = await creator(idlFactory, canisterID);
+    const balance = await create.wallet_balance();
+
+    return bigint2string(balance);
+};
+
+// tokens balance
+export const get_tokens_balance = async (
+    identity: ConnectedIdentity,
+    arg: {
+        owner: string;
+        subaccount?: string;
+    },
+): Promise<{ canister_id: string; balance: string }[]> => {
+    const { creator } = identity;
+    const create: _SERVICE = await creator(idlFactory, canisterID);
+
+    const balances = await create.tokens_balance_of({
+        owner: string2principal(arg.owner),
+        subaccount: wrapOptionMap(arg.subaccount, hex2array),
+    });
+
+    return balances.map((d) => {
+        return {
+            canister_id: d[0].toString(),
+            balance: bigint2string(d[1]),
+        };
+    });
 };
